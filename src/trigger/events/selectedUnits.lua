88c82e79ccb390cb9e96a0cdd538bbb6e2ee9358
Sergey Bugrov <nelloy93@yandex.ru>
2019-08-22 15:46:32 +0300

    work

---
+++ src/trigger/events/selectedUnits.lua
@@ -0,0 +1,73 @@
+---@type Trigger
+local Trigger = require('trigger.trigger')
+---@type UnitEvent
+local UnitEvent = require('trigger.events.unitEvent')
+
+local SelectedUnits = {}
+
+local function unitSelected()
+    local unit = GetTriggerUnit()
+    local player = GetTriggerPlayer()
+
+    local list = SelectedUnits[player]
+    local already_selected = false
+    for i = 1, #list do
+        if unit == list[i] then
+            already_selected = true
+            break
+        end
+    end
+
+    if not already_selected then
+        table.insert(list, unit)
+    end
+end
+
+local function unitDeselected()
+    local unit = GetTriggerUnit()
+    local player = GetTriggerPlayer()
+
+    local list = SelectedUnits[player]
+    local pos = -1
+    for i = 1, #list do
+        if unit == list[i] then
+            pos = i
+            break
+        end
+    end
+
+    if pos > 0 then
+        table.remove(list, pos)
+    end
+end
+
+function SelectedUnits.init()
+    for i = 0, bj_MAX_PLAYER_SLOTS do
+        SelectedUnits[Player(i)] = {}
+    end
+    ---@type Trigger
+    local selection_trigger = UnitEvent.getTrigger("AnyUnitSelected")
+    selection_trigger:addAction(unitSelected, nil)
+
+    ---@type Trigger
+    local deselection_trigger = UnitEvent.getTrigger("AnyUnitDeselected")
+    deselection_trigger:addAction(unitDeselected, nil)
+end
+
+---@param player wc3_Player
+---@return wc3_Unit[]
+function SelectedUnits.get(player)
+    local copy = {}
+    for i = 1, #SelectedUnits[player] do
+        table.insert(copy, SelectedUnits[i])
+    end
+    return copy
+end
+
+---@param player wc3_Player
+---@return integer
+function SelectedUnits.count(player)
+    return #SelectedUnits[player]
+end
+
+return SelectedUnits
--- src/trigger/events/unitEvent.lua
+++ src/trigger/events/unitEvent.lua
@@ -9,26 +9,35 @@ local UnitEvent = {
 local initialized = false
 function UnitEvent.init()
     if initialized then return nil end
+    
     ---@type Trigger
-    UnitEvent.__triggers.AnyUnitDie = Trigger.new()
-    UnitEvent.__triggers.AnyUnitDie:addEvent_AnyUnit("Death")
+    UnitEvent.__triggers["AnyUnitDie"] = Trigger.new()
+    UnitEvent.__triggers["AnyUnitDie"]:addEvent_AnyUnit("Death")
     ---@type Trigger
-    UnitEvent.__triggers.AnyUnitStartChannelAbility = Trigger.new()
-    UnitEvent.__triggers.AnyUnitStartChannelAbility:addEvent_AnyUnit("SpellChannel")
+    UnitEvent.__triggers["AnyUnitStartChannelAbility"] = Trigger.new()
+    UnitEvent.__triggers["AnyUnitStartChannelAbility"]:addEvent_AnyUnit("SpellChannel")
     ---@type Trigger
-    UnitEvent.__triggers.AnyUnitIssuedAnyOrder = Trigger.new()
-    UnitEvent.__triggers.AnyUnitIssuedAnyOrder:addEvent_AnyUnit("IssuedOrder")
-    UnitEvent.__triggers.AnyUnitIssuedAnyOrder:addEvent_AnyUnit("IssuedOrderTarget")
-    UnitEvent.__triggers.AnyUnitIssuedAnyOrder:addEvent_AnyUnit("IssuedOrderUnitTarget")
-    UnitEvent.__triggers.AnyUnitIssuedAnyOrder:addEvent_AnyUnit("IssuedOrderPointTarget")
+    UnitEvent.__triggers["AnyUnitIssuedAnyOrder"] = Trigger.new()
+    UnitEvent.__triggers["AnyUnitIssuedAnyOrder"]:addEvent_AnyUnit("IssuedOrder")
+    UnitEvent.__triggers["AnyUnitIssuedAnyOrder"]:addEvent_AnyUnit("IssuedOrderTarget")
+    UnitEvent.__triggers["AnyUnitIssuedAnyOrder"]:addEvent_AnyUnit("IssuedOrderUnitTarget")
+    UnitEvent.__triggers["AnyUnitIssuedAnyOrder"]:addEvent_AnyUnit("IssuedOrderPointTarget")
+    ---@type Trigger
+    UnitEvent.__triggers["AnyUnitSelected"] = Trigger.new()
+    UnitEvent.__triggers["AnyUnitSelected"]:addEvent_AnyUnit("Selected")
+    ---@type Trigger
+    UnitEvent.__triggers["AnyUnitDeselected"] = Trigger.new()
+    UnitEvent.__triggers["AnyUnitDeselected"]:addEvent_AnyUnit("Deselected")
 
     initialized = true
 end
 
 ---@alias UnitEventName string
 ---| '"AnyUnitDie"'
----| '"AnyUnitStartChannelAbility"'
 ---| '"AnyUnitIssuedAnyOrder"'
+---| '"AnyUnitStartChannelAbility"'
+---| '"AnyUnitSelected"'
+---| '"AnyUnitDeselected"'
 
 ---@param name UnitEventName
 function UnitEvent.getTrigger(name)
