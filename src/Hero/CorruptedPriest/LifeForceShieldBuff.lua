6a474eff375acad47de60cc6969decd6616c5ac2
nelloy-git <nelloy93@yandex.ru>
2020-10-28 19:58:32 +0300

    work

--- src/Hero/CorruptedPriest/LifeForceShieldBuff.lua
+++ src/Hero/CorruptedPriest/LifeForceShieldBuff.lua
@@ -8,34 +8,66 @@ local Icon = AssetLib.IconDefault.BTNAbsorbMagic or error('')
 ---@type BuffLib
 local BuffLib = require(LibList.BuffLib) or error('')
 local BuffType = BuffLib.Type or error('')
+---@type HandleLib
+local HandleLib = require(LibList.HandleLib) or error('')
+local Effect = HandleLib.Effect or error('')
+
+--==========
+-- Settings
+--==========
+
+local model = 'Abilities\\Spells\\Human\\ManaFlare\\ManaFlareTarget.mdl'
+local model_r = 1
+local model_g = 1
+local model_b = 1
+local model_a = 0.3
+
+--======
+-- Data
+--======
+
+local stored_shield = {}
+local effect = {}
+
 
 --========
 -- Module
 --========
 
 local LifeForceShieldBuff = BuffType.new('Life Force Shield Effect')
-local stored_shield = {}
 
 ---@param buff Buff
 function LifeForceShieldBuff:onStart(buff)
+    local target = buff:getTarget()
+
     stored_shield[buff] = buff:getUserData()
-    BuffLib.addShield(stored_shield[buff], buff:getTarget())
+    effect[buff] = Effect.new(model, target:getX(), target:getY(), target:getZ())
+    effect[buff]:setScale(4)
+    effect[buff]:setColor(model_r, model_g, model_b, model_a)
+
+    BuffLib.addShield(stored_shield[buff], target)
 end
 
 ---@param buff Buff
 function LifeForceShieldBuff:onLoop(buff)
+    local target = buff:getTarget()
+    effect[buff]:setPos(target:getX(), target:getY(), target:getZ())
 end
 
 ---@param buff Buff
 function LifeForceShieldBuff:onCancel(buff)
     BuffLib.addShield(-stored_shield[buff], buff:getTarget())
     stored_shield[buff] = nil
+    effect[buff]:destroy()
+    effect[buff] = nil
 end
 
 ---@param buff Buff
 function LifeForceShieldBuff:onFinish(buff)
     BuffLib.addShield(-stored_shield[buff], buff:getTarget())
     stored_shield[buff] = nil
+    effect[buff]:destroy()
+    effect[buff] = nil
 end
 
 ---@param buff Buff
